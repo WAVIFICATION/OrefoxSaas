93443738462912dfda86929a78fa9e4ac04bcda8
Nikhil Nair <n10400079@qut.edu.au>
2020-04-07 19:29:03 +1000

    Initial Commit
    Flask Login

---
+++ www/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def main():
+    return render_template('templates/index1.html', title ="Home")
+
+
+if __name__ =="__main__":
+    app.run()
